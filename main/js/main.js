874be1d264574f91b6da23d7cb39ced20021a6bd
Scrish96 <ceoh.96@gmail.com>
2022-05-30 13:21:54 -0500

    mi primer commit de fixmort

---
+++ main/js/main.js
@@ -0,0 +1,79 @@
+
+function moto_x_repa(){
+    jQuery(document).ready(function () {
+      $('#content').load('./pages/mot_x_repa.html');
+    });
+}
+
+function moto_repa(){
+    $(document).ready(function () {
+      $('#content').load('./pages/moto_repa.html');
+    });
+}
+
+function trac_moto(){
+    $(document).ready(function () {
+      $('#content').load('./pages/trac_moto.html');
+    });
+}
+
+function esta_mate(){
+    $(document).ready(function () {
+      $('#content').load('./pages/esta_mate.html');
+    });
+}
+
+function web_design(){
+    $(document).ready(function () {
+      $('#content').load('./pages/web_design.html');
+    });
+}
+
+function reg_usuario(){
+  $(document).ready(function () {
+    $('#content').load('./pages/registrar-usuario.html');
+    
+  });
+}
+
+function edit_usuario(){
+  $(document).ready(function () {
+    $('#content').load('./pages/editar-usuario.html');
+
+  });
+}
+
+function bus_usuario(){
+  $(document).ready(function () {
+    $('#content').load('./pages/buscar-usuario.html');
+
+  });
+}
+
+function reg_persona(){
+  $(document).ready(function () {
+    $('#content').load('./pages/reg-persona.html');
+    
+  });
+}
+
+function edit_persona(){
+  $(document).ready(function () {
+    $('#content').load('./pages/editar-persona.html');
+
+  });
+}
+
+function bus_persona(){
+  $(document).ready(function () {
+    $('#content').load('./pages/buscar-persona.html');
+
+  });
+}
+
+function reg_cotiz(){
+  $(document).ready(function () {
+    $('#content').load('./pages/reg-cotiz.html');
+
+  });
+}
