923e142c2827c784326984e7ce988397e0ef9df6
renzoxd245 <69137794+renzoxd245@users.noreply.github.com>
2022-06-14 18:41:12 -0500

    commit on 'main'

--- main/js/main.js
+++ main/js/main.js
@@ -1,78 +1,95 @@
-function detalle_ot(){
-    $(document).ready(function () {
-      $('#content').load('./pages/detalle-ot.html');
-    });
-}
-function mod_piezas_existentes(){
-  $(document).ready(function () {
-    $('#content').load('./pages/mod-piezas-existentes.html');
-  });
-}
-
-function buscar_piezas(){
-  $(document).ready(function () {
-    $('#content').load('./pages/buscar-pieza.html');
-  });
-}
-
-function Agregar_piezas(){
-    $(document).ready(function () {
-      $('#content').load('./pages/agregar-pieza.html');
+function detalle_ot() {
+    $(document).ready(function() {
+        $('#content').load('./pages/detalle-ot.html');
     });
 }
 
-function reg_usuario(){
-  $(document).ready(function () {
-    $('#content').load('./pages/registrar-usuario.html');
-    
-  });
+function mod_piezas_existentes() {
+    $(document).ready(function() {
+        $('#content').load('./pages/mod-piezas-existentes.html');
+    });
 }
 
-function edit_usuario(){
-  $(document).ready(function () {
-    $('#content').load('./pages/editar-usuario.html');
-
-  });
+function buscar_piezas() {
+    $(document).ready(function() {
+        $('#content').load('./pages/buscar-pieza.html');
+    });
 }
 
-function bus_usuario(){
-  $(document).ready(function () {
-    $('#content').load('./pages/buscar-usuario.html');
-
-  });
+function Agregar_piezas() {
+    $(document).ready(function() {
+        $('#content').load('./pages/agregar-pieza.html');
+    });
 }
 
-function reg_persona(){
-  $(document).ready(function () {
-    $('#content').load('./pages/reg-persona.html');
-    
-  });
+function reg_usuario() {
+    $(document).ready(function() {
+        $('#content').load('./pages/registrar-usuario.html');
+
+    });
 }
 
-function edit_persona(){
-  $(document).ready(function () {
-    $('#content').load('./pages/editar-persona.html');
+function edit_usuario() {
+    $(document).ready(function() {
+        $('#content').load('./pages/editar-usuario.html');
 
-  });
+    });
 }
 
-function bus_persona(){
-  $(document).ready(function () {
-    $('#content').load('./pages/buscar-persona.html');
+function bus_usuario() {
+    $(document).ready(function() {
+        $('#content').load('./pages/buscar-usuario.html');
 
-  });
+    });
 }
 
-function reg_cotiz(){
-  $(document).ready(function () {
-    $('#content').load('./pages/reg-cotiz.html');
+function reg_persona() {
+    $(document).ready(function() {
+        $('#content').load('./pages/reg-persona.html');
 
-  });
+    });
 }
 
-function buscar_ot(){
-  $(document).ready(function () {
-    $('#content').load('./pages/buscar-orden-ot.html');
+function edit_persona() {
+    $(document).ready(function() {
+        $('#content').load('./pages/editar-persona.html');
 
-  });
+    });
+}
+
+function bus_persona() {
+    $(document).ready(function() {
+        $('#content').load('./pages/buscar-persona.html');
+
+    });
+}
+
+function reg_cotiz() {
+    $(document).ready(function() {
+        $('#content').load('./pages/reg-cotiz.html');
+
+    });
+}
+
+function buscar_ot() {
+    $(document).ready(function() {
+        $('#content').load('./pages/buscar-orden-ot.html');
+
+    });
+}
+
+
+
+function edit_empresa() {
+    $(document).ready(function() {
+        $('#content').load('./pages/editar-empresa.html');
+
+    });
+}
+
+function bus_empresa() {
+    $(document).ready(function() {
+        $('#content').load('./pages/buscar-empresa.html');
+
+    });
 }
